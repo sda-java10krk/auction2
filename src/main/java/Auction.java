9ebc9e0f62bf7821c2100a3ba3d762fd02386748
JD <donimirski2@gmail.com>
2018-07-20 18:47:47 +0200

    master with JD added

---
+++ src/main/java/Auction.java
@@ -0,0 +1,57 @@
+import java.math.BigDecimal;
+import java.util.Locale;
+
+
+public class Auction {
+    private String title;
+    private String description;
+    private Category category;
+    private User seller;
+    private User winner;
+    private BigDecimal price;
+
+    public Auction(String title, String description, Category category, User seller, User winner, BigDecimal price) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.seller = seller;
+        this.winner = winner;
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public void setWinner(User winner) {
+        this.winner = winner;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Auction{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", category=" + category +
+                ", seller=" + seller +
+                ", winner=" + winner +
+                ", price=" + price +
+                '}';
+    }
+}
