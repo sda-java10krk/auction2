1edf9a5c11602c478b127a38c58eae90a4afd76e
anmat <ankaem25@gmail.com>
2018-07-20 18:58:28 +0200

    commit

--- src/main/java/srallegro/Category.java
+++ src/main/java/srallegro/Category.java
@@ -1,4 +1,28 @@
 package srallegro;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Category {
+    private String name;
+    private Set<Auction> auctions;
+    private Set<Category> subcategories;
+
+    public Category(String name){
+        this.name = name;
+        this.auctions = new TreeSet<>();
+        this.subcategories = new TreeSet<>();
+    }
+
+    public void addAuction(Auction auction){
+        this.auctions.add(auction);
+    }
+
+    public void addSubcategory(Category category){
+        this.subcategories.add(category);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
