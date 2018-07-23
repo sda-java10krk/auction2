ea453a4fa62547618c746a49485df6d479cad195
Artur <artois.gb@gmail.com>
2018-07-23 20:44:21 +0200

    change in Main, User

--- src/main/java/srallegro/Main.java
+++ src/main/java/srallegro/Main.java
@@ -4,12 +4,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
 
-        User first = User.personBuilder().withName(name).build();
+        public static void main(String[] args) {
 
-        System.out.println(first.toString());
+            DatabaseUsers databaseUsers = new DatabaseUsers();
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Dzien dobty co chcesz szroibc");
+            System.out.println("1 - zalogowac sie");
+            System.out.println("2 - zarejestrowac");
+            String answer = scanner.nextLine();
+
+            switch (answer) {
+                case ("2"):
+                    User user = RegisterUser.createUser();
+                    databaseUsers.addUser(user);
+                    break;
+            }
+
+
+        }
     }
-}
+
