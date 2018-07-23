d4c09fc06368b706e29c8de65575b4b7d2defe97
JD <donimirski2@gmail.com>
2018-07-23 20:30:26 +0200

    ssfdfdffd

--- src/main/java/srallegro/Main.java
+++ src/main/java/srallegro/Main.java
@@ -1,15 +1,26 @@
 package srallegro;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
+        Integer auctionNumber = 0;  //bedzie zwiekszany przy kazdej dodanej aukcji. tylko na razie nie działa
 
-        User first = User.personBuilder().withName(name).build();
+        HashSet<User> allUsers = new HashSet<>();
+        Map<String, User> usersByName = new HashMap<>();
+        User tempUser = new User ("Piotr", "Nowak", 0, "", "", "", "Nickname" );
+        usersByName.put(tempUser.getNick(), tempUser); //tak mozna latwo dodawac nowych
 
-        System.out.println(first.toString());
+        //w jakiej kolekcji beda siedziec wszystkie kategorire?
+        Map<String, Category> categoriesByName = new HashMap<>();
+        Category tempCat = new Category ("Huśtawki pneumatyczne");
+        categoriesByName.put(tempCat.getName(), tempCat);
+
+        Map<Integer, Auction> auctionsByNumber = new HashMap<>();
+        //Auction tempAuction = new Auction(null, null, null, null, null, null, auctionNumber, 0);
     }
 }
