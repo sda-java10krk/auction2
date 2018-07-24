5c6e37029f3582b3a6982978700cf05d8b2ec891
anmat <ankaem25@gmail.com>
2018-07-24 20:31:30 +0200

    category

--- src/main/java/srallegro/Database.java
+++ src/main/java/srallegro/Database.java
@@ -12,6 +12,8 @@ public class Database {
     public static Map<String, User> usersByName = new HashMap<>();
     public static Set<Auction> allAuctions = new HashSet<>();
 
+    public static Category category = new Category("All Categories");
+
 
     protected void addUser(User user) throws FileNotFoundException {
         String  userNick = user.getNick();
