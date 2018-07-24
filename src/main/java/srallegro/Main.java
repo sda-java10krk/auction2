cf89e7cd8f18601c0d19f459c6ab31d11fbd761b
JD <donimirski2@gmail.com>
2018-07-24 19:53:20 +0200

    sjf

--- src/main/java/srallegro/Main.java
+++ src/main/java/srallegro/Main.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void printMenu() {
+        System.out.println("Co chcesz zrobić? \n  1. Wystaw przedmiot \n 2. Pokaż aukcje wg kategorii ");
+    }
+
         public static void main(String[] args) {
             Database database = new Database();  // po co mi to
             Scanner sc = new Scanner(System.in);
-            User currentUser;
+            User currentUser = null;
             System.out.println("1 - zaloguj się, 2 - zarejestruj się");
             int menuChoice = sc.nextInt();
 
@@ -24,7 +28,30 @@ public class Main {
             } else if (menuChoice == 2) {
                 RegisterUser.createUser();
                 System.out.println("Zarejestrowano użytkownika. Uruchom program ponownie i zaloguj się");
+            } else {
+                System.out.println("Do widzenia");
             }
+
+            while (currentUser != null) {
+                printMenu();
+                menuChoice = sc.nextInt();
+                if (menuChoice == 1) {
+                    System.out.println("Podaj tytuł aukcji");
+                    String title = sc.next();
+                    System.out.println("Podaj opis");
+                    String description = sc.next();
+                    System.out.println("Podaj cenę wywoławczą");
+                    double price = sc.nextDouble();
+                    System.out.println("Wybierz kategorię");
+                    //CategoryView.printCategories();  //jak wyświetlić? jak wybrac kategorie?
+                    String chosenCat = sc.next();
+                    Category cat = new Category ("Robocza kategoria");
+                    AuctionController.createAuction(currentUser, title, description, cat, price);
+                }
+            }
+
+
+
         }
     }
 
