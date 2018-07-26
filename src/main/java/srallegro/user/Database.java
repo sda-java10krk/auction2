11eb1af8e31d51b9a36dad833ce60f4e78234de3
JD <donimirski2@gmail.com>
2018-07-26 18:55:45 +0200

    singleton work

--- src/main/java/srallegro/user/Database.java
+++ src/main/java/srallegro/user/Database.java
@@ -1,4 +1,5 @@
 package srallegro.user;
+import srallegro.Category;
 import srallegro.auction.Auction;
 
 import java.io.*;
@@ -6,14 +7,50 @@ import java.util.*;
 
 
 public class Database {
-    public static Map<String, User> usersByName = new HashMap<>();
-    public static Set<Auction> allAuctions = new HashSet<>();
+
+    private static Database instance;
+
+    private Database(){}
+
+    public static synchronized Database getInstance() {
+        if(instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    private static Map<String, srallegro.user.User> allUsersByNickname = new HashMap<>();
+    private static Map<String, Category> allCategoriesByName = new HashMap<>();
+    private static Map <String, Auction> allAuctionsByNumber = new HashMap<>();
+
+
+
+    //regarding Users
+    public static Map<String, srallegro.user.User> getAllUsersByNickname() {
+        return allUsersByNickname;
+    }
+
+    public static void addUserToAllUsers (srallegro.user.User user) {
+        allUsersByNickname.put(user.getNick(), user);
+    }
+
+    public static User getUserByNickname (String nickname) {
+        try {
+            User user = allUsersByNickname.get(nickname);
+            return user;
+        } catch (NullPointerException npe) {
+            System.out.println("Nie ma takiego użytkownika");
+        }
+        return null;
+    }
+
+
 
 
     public static void addUser(User user) throws IOException {
+        Database database = Database.getInstance();
         String userNick = user.getNick();
-        usersByName.put(userNick, user);
+        addUserToAllUsers(user);
         SaveUserOnDisk.writeCsvFile("databaseUser.csv", user);
-
     }
 }
