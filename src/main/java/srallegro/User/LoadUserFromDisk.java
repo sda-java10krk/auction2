f29dcfbc06e032efe855c5ca47f345981d8e9c21
Artur <artois.gb@gmail.com>
2018-07-26 18:11:45 +0200

    merge

--- src/main/java/srallegro/User/LoadUserFromDisk.java
+++ src/main/java/srallegro/User/LoadUserFromDisk.java
@@ -1,8 +1,44 @@
 package srallegro.User;
 
-public class LoadUserFromDisk {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-    private static final String COMMA_SEPARATOR = ",";
-    
+public class LoadUserFromDisk {
+    private static LoadUserFromDisk instance;
+
+    private  LoadUserFromDisk(){
+    }
+
+    private static LoadUserFromDisk getInstance() {
+        if (instance == null) {
+            instance = new LoadUserFromDisk();
+        }
+        return instance;
+    }
+
+    private static final String COMMA_SEPARATOR = ",";private static final int USER_ADDRESS =3;
+    private static final String NEW_LINE_SEPARATOR = "\n";
+    private static final int USER_NAME =0;
+    private static final int USER_LASTNAME =1;
+    private static final int USER_BIRTHDAY =2;
+
+    private static final int USER_MAIL =4;
+    private static final int USER_PASSWORD =5;
+    private static final int USER_NICK =6;
+
+    public static void readFileCSV(String fileName, User user){
+
+        BufferedReader filereader =null;
+
+        String line ="";
+        try {
+            filereader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }k
+    }
+
+
 
 }
