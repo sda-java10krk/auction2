17a6f1fc63cb9b1db0b45ded8464b2b464cf087c
JD <donimirski2@gmail.com>
2018-07-31 20:44:30 +0200

    fix

--- src/main/java/srallegro/auction/LoadAuctionFromDisk.java
+++ src/main/java/srallegro/auction/LoadAuctionFromDisk.java
@@ -49,11 +49,17 @@ public class LoadAuctionFromDisk {
                     Auction auction = new Auction(data[AUCTION_TITLE], data[AUCTION_DESCRIPTION],new Category(data[AUCTION_CATEGORY]), Database.getInstance().getAllUsersByNickname().get(data[AUCTION_NAME_SELLER]),Database.getInstance().getAllUsersByNickname().get(data[AUCTION_NAME_WINNER]),
                      new BigDecimal(data[AUCTION_PRICE]), Integer.parseInt(data[AUCTION_NuMBER_AUCTION]), Integer.parseInt(data[AUCTION_BIDS]));
                     database.addAuctionToAllAuctions(auction);
+                    database.getAllUsersByNickname().get(data[AUCTION_NAME_SELLER]).getMySellingList().add(auction);
+                    if (auction.getBids() >=3) {
+                        database.getAllUsersByNickname().get(data[AUCTION_NAME_WINNER]).getMyWonList().add(auction);
+                    }
                 }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
